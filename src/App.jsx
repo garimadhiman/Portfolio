dbaa8982d5f8f57ee01bd77aa84361026a3bd325
garimadhiman <97743570+garimadhiman@users.noreply.github.com>
2023-12-30 23:43:54 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Hero } from "./components/Hero/Hero";
+import { Interests } from "./components/Interests/Interests";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Interests />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+              As a frontend developer, I bring expertise in crafting responsive and optimized websites, ensuring seamless user experiences across various devices.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+              I am actively exploring backend development, dedicated to acquiring a comprehensive understanding of its principles and best practices 
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+              Embarking on my design journey, I am in the early stages of honing my skills and knowledge in the field.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
